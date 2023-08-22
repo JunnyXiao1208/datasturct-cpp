0f76d654a2c2ca267cb077eb7c9850ec8338026f
JunnyXiao <jielunzhou276@gmail.com>
2023-08-22 20:50:05 +0800

    更改优化的幂函数算法内容

--- 绪论/优化的幂函数算法.cpp
+++ 绪论/优化的幂函数算法.cpp
@@ -7,3 +7,17 @@ _int64 power2(int n)
         return 1;
     return (n & 1) ? sqr(power2(n >> 1)) << 1 : sqr(power2(n >> 1));
 }
+
+//通用 求a^n
+_int64 power(_int64 a,int n){
+    _int64 pow = 1;//处理a^0
+    _int64 p = a;
+    while(n>0){
+        if(n&1)
+            pow*=p;
+        n>>=1;
+        p*=p;
+    }
+    return pow;
+
+}
